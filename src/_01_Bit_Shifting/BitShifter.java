13b48eef5e7e15c563e95eab2de6bcf65e8fa46f
Your Name <you@example.com>
2019-06-02 12:59:53 -0700

    BinaryPrinter WIP

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,14 +4,17 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
+		int Bnum = 0b100;
 		int numShifted = num << 1;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
+		System.out.println(numShifted);
+		numShifted = 0b1000;
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
 		
 		// 4. Try shifting num 3 places.
-		
+		numShifted = num << 3;
+		System.out.println(numShifted);
 		// FYI: Binary values can be shifted to the right as well using the >> operator.	
 	}
 }
--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -4,20 +4,39 @@ public class BinaryPrinter {
 	//Complete the methods below so they print the passed in parameter in binary.
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
-	
+	public static void main(String[] args) {
+		BinaryPrinter bp = new BinaryPrinter();
+		bp.printIntBinary(63);
+	}
 	public void printByteBinary(byte b) {
-		
+
 	}
 	
 	public void printShortBinary(short s) {
-		
+
 	}
 	
 	public void printIntBinary(int i) {
-		
+		String b = "";
+		int j = 1;
+		int count = 0;
+		while(i > j) {
+			j *= 2;
+			count++;
+		}
+		for (int k = count; k > 0; k--) {
+			if ((i - 2^count) < 0) {
+				break;
+			}
+			if ((i - 2^count) % 2 != 1) {
+				i = i - 2^count;
+				b += "1";
+			}
+		}
 	}
 	
 	public void printLongBinary(long l) {
 		
 	}
+
 }
