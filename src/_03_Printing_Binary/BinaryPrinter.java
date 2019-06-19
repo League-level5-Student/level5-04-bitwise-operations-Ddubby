70cd198eac2a06212139b9cf7e9a9d721c5423b7
League Student <league@WorthyMini-6.attlocal.net>
2019-06-18 18:27:00 -0700

    Base64Decoder WIP

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -1,42 +1,77 @@
 package _03_Printing_Binary;
 
 public class BinaryPrinter {
-	//Complete the methods below so they print the passed in parameter in binary.
-	//Don't be afraid to use the methods that are already complete to finish the others.
-	//Create a main method to test your methods.
+	// Complete the methods below so they print the passed in parameter in binary.
+	// Don't be afraid to use the methods that are already complete to finish the
+	// others.
+	// Create a main method to test your methods.
+	
 	public static void main(String[] args) {
 		BinaryPrinter bp = new BinaryPrinter();
-		bp.printIntBinary(63);
+		byte b = 13;
+		short s = 13;
+		long l = 13;
+		bp.printIntBinary(13);
+		System.out.println();
+		bp.printByteBinary(b);
+		System.out.println();
+		bp.printShortBinary(s);
+		System.out.println();
+		bp.printLongBinary(l);
 	}
+
 	public void printByteBinary(byte b) {
-
+		String s = "";
+		if (b > 0) {
+			if ((b & 1) == 1) {
+				s = "1";
+			} else {
+				s = "0";
+			}
+			printByteBinary((byte)(b >> 1));
+			System.out.print(s);
+		}
 	}
-	
+
 	public void printShortBinary(short s) {
+		String a = "";
+		if (s > 0) {
+			if ((s & 1) == 1) {
+				a = "1";
+			} else {
+				a = "0";
+			}
+			printShortBinary((short)(s >> 1));
+			System.out.print(a);
+		}
+	}
 
-	}
-	
 	public void printIntBinary(int i) {
-		String b = "";
-		int j = 1;
-		int count = 0;
-		while(i > j) {
-			j *= 2;
-			count++;
-		}
-		for (int k = count; k > 0; k--) {
-			if ((i - 2^count) < 0) {
-				break;
+		String s = "";
+		if (i > 0) {
+			if (i % 2 == 1) {
+				i = (i - 1) / 2;
+				s = "1";
+			} else {
+				i = i / 2;
+				s = "0";
 			}
-			if ((i - 2^count) % 2 != 1) {
-				i = i - 2^count;
-				b += "1";
-			}
+			printIntBinary(i);
+			System.out.print(s);
 		}
 	}
-	
+
 	public void printLongBinary(long l) {
-		
+		String s = "";
+		if (l > 0) {
+			if ((l & 1) == 1) {
+				s = "1";
+			} else {
+				s = "0";
+			}
+			printByteBinary((byte)(l >> 1));
+			System.out.print(s);
+		}
 	}
 
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -37,13 +37,31 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		byte n = 0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if (base64Chars[i] == c) {
+				n = (byte) i;
+			}
+		}
+		return n;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
+		byte[] bytes = new byte[4];
+		int a = 0;
+		for (char c : s.toCharArray()) {
+			byte n = 0;
+			for (int i = 0; i < base64Chars.length; i++) {
+				if (base64Chars[i] == c) {
+					n = (byte) i;
+				}
+			}
+			byte[0] = n;
+			
+		}
 		return null;
 	}
 	
